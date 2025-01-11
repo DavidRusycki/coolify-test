f32d1c083309b680e6153e1d159512b2a5282fe6
David Rusycki <davidrusyck@gmail.com>
2025-01-11 12:33:09 -0300

    feat: test ci cd

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Bun!");
+        return new Response("test ci cd!");
     },
 });
   
