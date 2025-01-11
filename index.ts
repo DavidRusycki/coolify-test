e8383a4400a98e9b7ebdbda32d04d6dcfd734315
David Rusycki <davidrusyck@gmail.com>
2025-01-11 15:18:00 -0300

    feat: add logs

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        console.log('new request: '+new Date());
+        console.log('request: '+new Date());
         return new Response("Bun Coolify!!");
     },
 });
